e1f573b03a4a15ab93450c05a62cc831b92e2c6a
mahanpalang <mahan.zanbi2020@gmail.com>
2016-07-31 13:56:56 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {ariya_moaven_bot},
+    sudo_users = {210632121},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
